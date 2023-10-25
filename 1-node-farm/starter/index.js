a14f3b9611911bcab134e20db477cc0fcfa20d24
Brenda <ziruhe@gmail.com>
2023-10-25 16:23:31 +0000

    Oct 25, 2023, 9:23 AM

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 
-const textIn = fs.readFile("./txt/input.txt", "utf-8", (err, data) => {
-  console.log(data);
-});
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
 const textOut = `This is what we know about avocado: ${textIn}. \nCreated on ${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textOut);
