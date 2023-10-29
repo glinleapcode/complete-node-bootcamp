417f8fca5cdc55cb5993126ab427e2b9a00732ab
Brenda <ziruhe@gmail.com>
2023-10-29 16:28:58 +0000

    Oct 29, 2023, 9:28 AM

--- 1-node-farm/10-28-23/index.js
+++ 1-node-farm/10-28-23/index.js
@@ -45,7 +45,7 @@ const server = http.createServer((req, res) => {
     fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
       const productData = JSON.parse(data);
       // console.log(productData);
-      res.writeHead(200, { "content-type": "application/json" });
+      res.writeHead(200, { "Content-type": "application/json" });
       res.end(data);
     });
   } else {
