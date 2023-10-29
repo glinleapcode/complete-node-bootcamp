820777319e81a06195d36dea1f29d678bac42586
Brenda <ziruhe@gmail.com>
2023-10-29 05:16:54 +0000

    Oct 28, 2023, 10:16 PM

--- 1-node-farm/10-28-23/index.js
+++ 1-node-farm/10-28-23/index.js
@@ -12,6 +12,16 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
     console.log(data2);
     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
       console.log(data3);
+      fs.writeFile(
+        "./txt/final.txt",
+        `${data2}\n\n\n${data3}`,
+        "utf-8",
+        (err) => {
+          console.log("your file has been written to final.txt");
+        }
+      );
     });
   });
 });
+
+console.log("done with files!");
