d513c615b82126705b06776e5518bd49e990883d
Brenda <ziruhe@gmail.com>
2023-10-29 05:40:14 +0000

    Oct 28, 2023, 10:40 PM

--- 1-node-farm/10-28-23/index.js
+++ 1-node-farm/10-28-23/index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require("http");
+
+///////////////////
+//Files
 
 // synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -7,21 +11,24 @@ const fs = require("fs");
 // console.log("File written!");
 
 //Non-blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
-      fs.writeFile(
-        "./txt/final.txt",
-        `${data2}\n\n\n${data3}`,
-        "utf-8",
-        (err) => {
-          console.log("your file has been written to final.txt");
-        }
-      );
-    });
-  });
-});
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile(
+//         "./txt/final.txt",
+//         `${data2}\n\n\n${data3}`,
+//         "utf-8",
+//         (err) => {
+//           console.log("your file has been written to final.txt");
+//         }
+//       );
+//     });
+//   });
+// });
 
-console.log("done with files!");
+// console.log("done with files!");
+
+/////////////////////////////////////////////////////
+// Server
