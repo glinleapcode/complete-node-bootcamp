c23853f7a71b18e49ef70dddba791ccf21aa7cff
Brenda <ziruhe@gmail.com>
2023-10-29 17:00:37 +0000

    Oct 29, 2023, 10:00 AM

--- 1-node-farm/10-28-23/index.js
+++ 1-node-farm/10-28-23/index.js
@@ -34,6 +34,10 @@ const url = require("url");
 
 /////////////////////////////////////////////////////
 // Server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === "/" || pathName === "/overview") {
@@ -41,12 +45,8 @@ const server = http.createServer((req, res) => {
   } else if (pathName === "/product") {
     res.end("This is the product page");
   } else if (pathName === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-      const productData = JSON.parse(data);
-      console.log(productData);
-      // res.writeHead(200, { "Content-type": "application/json" });
-      res.end(data);
-    });
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
