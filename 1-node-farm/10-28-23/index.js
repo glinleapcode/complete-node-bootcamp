aecc651ee98f424fde172b52b39943e371ce81d3
Brenda <ziruhe@gmail.com>
2023-10-29 04:59:58 +0000

    Oct 28, 2023, 9:59 PM

--- 1-node-farm/10-28-23/index.js
+++ 1-node-farm/10-28-23/index.js
@@ -5,3 +5,6 @@ const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 const textOut = `This is what we know about avocado: ${textIn}. \nCreated on ${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("File written!");
+
+//Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt");
