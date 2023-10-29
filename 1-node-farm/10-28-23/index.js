014231ba0229750ede4d5bcb27257303281b38cb
Brenda <ziruhe@gmail.com>
2023-10-29 05:45:56 +0000

    Oct 28, 2023, 10:45 PM

--- 1-node-farm/10-28-23/index.js
+++ 1-node-farm/10-28-23/index.js
@@ -33,6 +33,7 @@ const http = require("http");
 /////////////////////////////////////////////////////
 // Server
 const server = http.createServer((req, res) => {
+  console.log(req);
   res.end("Hello from the server!");
 });
 server.listen(8000, "127.0.0.1", () => {
